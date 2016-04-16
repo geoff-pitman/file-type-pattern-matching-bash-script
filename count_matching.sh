5b1dd62c6cbe4135f78289acdd5e1eaea6fc87fa
Geoffrey Pitman <gpitman379@gmail.com>
2016-04-15 21:19:55 -0400

    Update count_matching.sh

--- count_matching.sh
+++ count_matching.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
-# Geoffrey Pitman
-# shell3_2016.sh -- D. Parson solution to assignment 3, CSC 352 Unix
-#   Spring 2016. This program accepts a path to a DIRECTORY as its first
+#   This program accepts a path to a DIRECTORY as its first
 #   command line argument, followed by one or more string PATTERNs
 #   as the remaining command line arguments.
+#
+#   It must verify that it has at least 2 initial command line arguments
+#   (the DIRECTORY and at least one PATTERN), and that the DIRECTORY is
+#   in fact a directory. If any of these conditions fails, report an error
+#   TO STANDARD ERROR and exits the script with an exit status of 1.
+#   Otherwise proceed:
+#
+#   For each string pattern, it calls a shell function search()
+
+#   search() accepts the DIRECTORY and current PATTERN as function arguments.
+#       Find all of the *regular files* within and below the DIRECTORY
+#       For each of those regular files
+#           Run the file command and egrep for the pattern.
+#           If the output from the file command satisfies the
+#           egrep pattern, then:
+#               Add 1 to a counter for the number of file types matching PATTERN.
+#               Accumulate the number of lines in the file itself
+#                   in a variable. 
+#                   Accumulate the number of lines in the file.
+#               Accumulate the number of words in the file itself in a
+#                   different variable.
+#               Accumulate the number of characters in the file itself in a
+#                   third variable.
+#       The search() function returns these four counters to the main
+#           shell code that calls the function.
+
 
 function search()
 {
